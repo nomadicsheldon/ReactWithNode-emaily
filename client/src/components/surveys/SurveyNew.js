df9d8da464a92ad7a93192469842c965709a60fc
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-09-22 19:21:15 +0530

    client side implementation of survey complete

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,39 @@
+// SurveyNew shows SurveyForm and SurveyFormReview
+import React, { Component } from "react";
+import { reduxForm } from "redux-form";
+import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
+
+class SurveyNew extends Component {
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { new: true };
+  // }
+
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+// this is added for - when user click on cancel button then we want to clear form
+export default reduxForm({
+  form: "surveyForm",
+})(SurveyNew);
